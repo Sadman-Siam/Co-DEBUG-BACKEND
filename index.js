569b1c732f0ac9d738fd744b63914734d6243dcd
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2026-02-13 07:10:07 +0600

    func: database connection active

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from "express";
-import MongoClient from "mongodb";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-async function ConnectMongoDb() {}
+async function ConnectMongoDb() {
+  const client = new MongoClient(process.env.MONGO_URL);
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.error(e);
+  } finally {
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Server Is Up and Running");
@@ -13,3 +24,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+ConnectMongoDb();
